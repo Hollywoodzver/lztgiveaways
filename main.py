f1ee681e7d3e2f7cba37369211710049b3f17d98
Hollywoodzver <111180653+Hollywoodzver@users.noreply.github.com>
2024-10-11 21:45:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from handlers import register_handlers 
+from create_by_time import register_repeat_handlers
+import config 
 
 
-API_TOKEN = 'your bot token'
-ADMIN_IDS = [123, 12345] #your admin ids
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=config.API_TOKEN)  # Используем API_TOKEN из config.py
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 dp.middleware.setup(LoggingMiddleware())
 
+register_handlers(dp, config.ADMIN_IDS)  # Используем ADMIN_IDS из config.py
+register_repeat_handlers(dp, config.ADMIN_IDS)
 
-register_handlers(dp, ADMIN_IDS)
-print('000000')
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
