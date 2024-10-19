14a2f5e2f836be821030482d3989050be340162b
Hollywoodzver <111180653+Hollywoodzver@users.noreply.github.com>
2024-10-19 11:08:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
+
 import logging
-from aiogram import Bot, Dispatcher, executor
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from handlers import register_handlers 
-from create_by_time import register_repeat_handlers
-import config 
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+import config
 
-
+from handlers import router
+from create_by_time import router_two
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=config.API_TOKEN)  # Используем API_TOKEN из config.py
 storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
-dp.middleware.setup(LoggingMiddleware())
+dp = Dispatcher(storage=storage)
 
-register_handlers(dp, config.ADMIN_IDS)  # Используем ADMIN_IDS из config.py
-register_repeat_handlers(dp, config.ADMIN_IDS)
+
+dp.include_router(router)
+dp.include_router(router_two)
+async def main():
+    # Запуск polling
+    await dp.start_polling(bot, skip_updates=True)
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
