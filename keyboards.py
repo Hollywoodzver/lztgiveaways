5c9ec41eb08a1a20a6cdd163b01a2fa5b7b1111f
Hollywood <111180653+Hollywoodzver@users.noreply.github.com>
2024-12-22 17:52:23 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -7,25 +7,30 @@ def get_main_keyboard():
     keyboard.add(KeyboardButton(text="📄 Создать новый розыгрыш"))
     keyboard.add(KeyboardButton(text="📄 Создавать розыгрыш каждые X дней"))
     keyboard.add(KeyboardButton(text="📄 Создать несколько розыгрышей"))
+    keyboard.add(KeyboardButton(text="📄 Создать из bb-code"))
+    keyboard.adjust(2)
     return keyboard.as_markup(resize_keyboard=True)
-
-
-def get_time_keyboard():
-    keyboard = ReplyKeyboardBuilder()
-    keyboard.add(KeyboardButton(text="Да, создать"))
-    keyboard.add(KeyboardButton(text="Нет"))
-    return keyboard.as_markup(resize_keyboard=True)
-
-def get_time1_keyboard():
-    keyboard = ReplyKeyboardBuilder()
-    keyboard.add(KeyboardButton(text="Да, создавать"))
-    keyboard.add(KeyboardButton(text="Отмена"))
-    return keyboard.as_markup(resize_keyboard=True)
-
-
 
 def inlinekey():
     keyboard = InlineKeyboardBuilder()
     keyboard.button(text="✅ Одобрить", callback_data="approve_")
     keyboard.button(text="❌ Отмена", callback_data="reject_")
+    return keyboard.as_markup()
+
+def bbkey():
+    keyboard = InlineKeyboardBuilder() 
+    keyboard.button(text="✅ Одобрить", callback_data="bbapprove_")
+    keyboard.button(text="❌ Отмена", callback_data="bbreject_")
+    return keyboard.as_markup()
+
+def cbtkey():
+    keyboard = InlineKeyboardBuilder()
+    keyboard.button(text="✅ Одобрить", callback_data="cbtapprove_")
+    keyboard.button(text="❌ Отмена", callback_data="cbtreject_")
+    return keyboard.as_markup()
+
+def masskey():
+    keyboard = InlineKeyboardBuilder()
+    keyboard.button(text="✅ Одобрить", callback_data="massapprove_")
+    keyboard.button(text="❌ Отмена", callback_data="massreject_")
     return keyboard.as_markup()
